a260c9d9d136c7b17b7650d11289e4b1b8b0abed
Lau <laststandrighthere@gmail.com>
2020-05-20 05:45:41 +0800

    net: Omit debug/tracing drivers when debug is disabled
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- net/netfilter/nf_internals.h
+++ net/netfilter/nf_internals.h
@@ -13,6 +13,23 @@ int nf_queue(struct sk_buff *skb, struct nf_hook_state *state,
 unsigned int nf_queue_nf_hook_drop(struct net *net);
 
 /* nf_log.c */
+#ifdef CONFIG_DEBUG_KERNEL
 int __init netfilter_log_init(void);
 
+/* core.c */
+void nf_hook_entries_delete_raw(struct nf_hook_entries __rcu **pp,
+				const struct nf_hook_ops *reg);
+int nf_hook_entries_insert_raw(struct nf_hook_entries __rcu **pp,
+				const struct nf_hook_ops *reg);
+#else
+static inline int __init netfilter_log_init(void)
+{
+	return 0;
+}
+
+void nf_hook_entries_delete_raw(struct nf_hook_entries __rcu **pp,
+				const struct nf_hook_ops *reg);
+int nf_hook_entries_insert_raw(struct nf_hook_entries __rcu **pp,
+				const struct nf_hook_ops *reg);
+#endif
 #endif
