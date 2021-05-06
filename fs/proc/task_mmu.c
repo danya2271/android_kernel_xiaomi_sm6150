7246c725d2caa634560f7737f16c94bd6382a584
Sultan Alsawaf <sultan@kerneltoast.com>
2021-05-06 00:00:32 -0700

    mm: Perform PID map reads on the little CPU cluster
    PID map reads for processes with thousands of mappings can be done
    extensively by certain Android apps, burning through CPU time on
    higher-performance CPUs even though reading PID maps is never a
    performance-critical task. We can relieve the load on the important CPUs
    by moving PID map reads to little CPUs via sched_migrate_to_cpumask_*().
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -230,6 +230,9 @@ static void *m_start(struct seq_file *m, loff_t *ppos)
 				       cpu_lp_mask);
 
 	down_read(&mm->mmap_sem);
+	sched_migrate_to_cpumask_start(to_cpumask(&priv->old_cpus_allowed),
+				       cpu_lp_mask);
+
 	hold_task_mempolicy(priv);
 	priv->tail_vma = get_gate_vma(mm);
 
