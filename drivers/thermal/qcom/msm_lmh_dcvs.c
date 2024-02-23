4fe66691663e2066790c3cc76ff74c27c5d9e5bd
danya2271 <danya2271@.yandex.ru>
2024-02-23 22:35:59 +0500

    Fix errors

--- drivers/thermal/qcom/msm_lmh_dcvs.c
+++ drivers/thermal/qcom/msm_lmh_dcvs.c
@@ -773,7 +773,6 @@ probe_exit:
 	INIT_LIST_HEAD(&hw->list);
 	list_add_tail(&hw->list, &lmh_dcvs_hw_list);
 	mutex_unlock(&lmh_dcvs_list_access);
-	lmh_debug_register(pdev);
 
 	ret = cpuhp_setup_state(CPUHP_AP_ONLINE_DYN, "lmh-dcvs/cdev:online",
 				limits_cpu_online, NULL);
