cfaea430886989d2dd4419a9bc6e0d06e2cd848c
danya2271 <dany2271@yandex.ru>
2024-08-25 23:32:30 +0500

    kernel: cgroup: Boost devfreq on app launch

--- kernel/cgroup/cgroup-v1.c
+++ kernel/cgroup/cgroup-v1.c
@@ -17,6 +17,7 @@
 #include <linux/binfmts.h>
 #include <linux/cpu_input_boost.h>
 #include <linux/gpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 #include <trace/events/cgroup.h>
 
 /*
@@ -547,6 +548,7 @@ static ssize_t __cgroup1_procs_write(struct kernfs_open_file *of,
         if (!ret && !threadgroup &&
                !memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
                task_is_zygote(task->parent)) {
+			devfreq_boost_kick_max(DEVFREQ_CPU_CPU_LLCC_BW, 1000);
                 cpu_input_boost_kick_max(1000);
 				gpu_input_boost_kick_max(1000);
         }
