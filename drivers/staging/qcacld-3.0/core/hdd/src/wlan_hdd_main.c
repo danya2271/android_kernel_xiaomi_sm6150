67151bb68d250660bd9d0f5bc359d8bb4b33ee79
danya2271 <dany2271@yandex.ru>
2025-04-25 18:57:11 +0500

    drivers: qcacld-3.0: Don't init debugfs

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -16067,12 +16067,6 @@ static QDF_STATUS hdd_qdf_init(void)
 	if (QDF_IS_STATUS_ERROR(status))
 		goto exit;
 
-	status = qdf_debugfs_init();
-	if (QDF_IS_STATUS_ERROR(status)) {
-		hdd_err("Failed to init debugfs; status:%u", status);
-		goto print_deinit;
-	}
-
 	qdf_lock_stats_init();
 	qdf_mem_init();
 	qdf_delayed_work_feature_init();
@@ -16115,8 +16109,6 @@ event_deinit:
 	qdf_mem_exit();
 	qdf_lock_stats_deinit();
 	qdf_debugfs_exit();
-print_deinit:
-	hdd_qdf_print_deinit();
 
 exit:
 	return status;
