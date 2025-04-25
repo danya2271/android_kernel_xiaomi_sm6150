28f0d15767f036496a6762f66f2fe8373c56cb54
danya2271 <dany2271@yandex.ru>
2025-04-25 18:31:19 +0500

    drivers: scsi-ufs: Guard ufshcd_cmd_log with CONFIG_FTRACE

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -768,7 +768,7 @@ static void ufshcd_print_cmd_log(struct ufs_hba *hba)
 static void ufshcd_cmd_log_init(struct ufs_hba *hba)
 {
 }
-
+#ifdef CONFIG_FTRACE
 static void __ufshcd_cmd_log(struct ufs_hba *hba, char *str, char *cmd_type,
 			     unsigned int tag, u8 cmd_id, u8 idn, u8 lun,
 			     sector_t lba, int transfer_len)
@@ -786,7 +786,7 @@ static void __ufshcd_cmd_log(struct ufs_hba *hba, char *str, char *cmd_type,
 
 	ufshcd_add_command_trace(hba, &entry, strcmp(cmd_type, "clk-gating"));
 }
-
+#endif
 static void ufshcd_dme_cmd_log(struct ufs_hba *hba, char *str, u8 cmd_id)
 {
 }
