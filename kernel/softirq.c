a946f31c692d9aa4bd16af3e4d85129faae2509b
danya2271 <dany2271@yandex.ru>
2025-04-25 22:14:17 +0500

    fixup! softirq: remove trace points for tasklet entry/exit

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -553,9 +553,7 @@ static __latent_entropy void tasklet_hi_action(struct softirq_action *a)
 				if (!test_and_clear_bit(TASKLET_STATE_SCHED,
 							&t->state))
 					BUG();
-				trace_tasklet_hi_entry(t->func);
 				t->func(t->data);
-				trace_tasklet_hi_exit(t->func);
 				tasklet_unlock(t);
 				continue;
 			}
